d2b0b53bdd0b0ca9274cd2ac2f154de61333f38a
Yohan Sandun Kumaranayake <yohan99ysk@gmail.com>
2022-06-04 21:32:38 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>LMS</title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+  
+</body>
+<script type="text/javascript" src="js/bootstrap.js"/>
+</html>
