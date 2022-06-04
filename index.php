fbe77c1d4a89d21aee71c873a29a8f8ec767abb4
Yohan Sandun Kumaranayake <yohan99ysk@gmail.com>
2022-06-04 22:12:46 +0530

    login added

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +15,7 @@
   <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 </head>
 <body>
-  
+  Home
 </body>
 <script type="text/javascript" src="js/bootstrap.js"/>
 </html>
